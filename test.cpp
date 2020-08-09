475c2b16de864d2af0e7f2b49368d861300f3253
MagicXin <x.yu2000@163.com>
2020-08-09 18:09:34 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,33 +1,9 @@
-<<<<<<< HEAD
 #include <bits/stdc++.h>
 using namespace std;
-
-
 
 int main(void)
 {
     string i = "13142223336";
 	cout << i.length() ;
 	return 0;
-=======
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-    int* p;
-    p = new int[10];
-
-    for(int i = 0 ; i < 10 ; i++){
-        p[i] = i;
-    }
-
-    for(int i = 0 ; i < 10 ; i++){
-        *p = i;
-        p++;
-        printf("p[%d]: %d;\n", i, p[i]);
-    }
-
-    //delete[] p;
-    return 0;
->>>>>>> b62fbd2b4c1f5a96e2ddf5436547f4bf7dfa9031
 }
