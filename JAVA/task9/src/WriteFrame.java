803714fefb2b097447b0af8027449f12aeceee2b
MagicXin <x.yu2000@163.com>
2020-11-19 17:43:12 +0800

    fix java-task9

--- JAVA/task9/src/WriteFrame.java
+++ JAVA/task9/src/WriteFrame.java
@@ -5,8 +5,8 @@ import java.io.*;
 
 public class WriteFrame extends JFrame implements ActionListener, Runnable {
 
-    JLabel num, name, sex, clas, cn, math, eng, pe, status;
-    JTextField num_t, name_t, sex_t, clas_t, cn_t, math_t, eng_t, pe_t;
+    JLabel num, name, sex, cls, cn, math, eng, pe, status;
+    JTextField num_t, name_t, sex_t, cls_t, cn_t, math_t, eng_t, pe_t;
     JButton write, clear;
 
     String dir = "Students.txt";
@@ -21,7 +21,7 @@ public class WriteFrame extends JFrame implements ActionListener, Runnable {
         num = new JLabel("学号：");
         name = new JLabel("姓名：");
         sex = new JLabel("性别：");
-        clas = new JLabel("班级：");
+        cls = new JLabel("班级：");
         cn = new JLabel("语文：");
         math = new JLabel("数学：");
         eng = new JLabel("英语：");
@@ -30,7 +30,7 @@ public class WriteFrame extends JFrame implements ActionListener, Runnable {
         num_t = new JTextField(5);
         name_t = new JTextField(5);
         sex_t = new JTextField(5);
-        clas_t = new JTextField(5);
+        cls_t = new JTextField(5);
         cn_t = new JTextField(5);
         math_t = new JTextField(5);
         eng_t = new JTextField(5);
@@ -42,8 +42,8 @@ public class WriteFrame extends JFrame implements ActionListener, Runnable {
         input.add(name_t);
         input.add(sex);
         input.add(sex_t);
-        input.add(clas);
-        input.add(clas_t);
+        input.add(cls);
+        input.add(cls_t);
         input.add(cn);
         input.add(cn_t);
         input.add(math);
@@ -85,13 +85,13 @@ public class WriteFrame extends JFrame implements ActionListener, Runnable {
             WriteIn();
         }
         if (e.getSource() == clear) {
-            Clear_Textfield();
+            Clear_text();
         }
     }
 
     void WriteIn() {
         if (num_t.getText().length() < 1 || name_t.getText().length() < 1 || sex_t.getText().length() < 1
-                || clas_t.getText().length() < 1 || cn_t.getText().length() < 1 || math_t.getText().length() < 1
+                || cls_t.getText().length() < 1 || cn_t.getText().length() < 1 || math_t.getText().length() < 1
                 || eng_t.getText().length() < 1 || pe_t.getText().length() < 1) {
             status.setText("数据不完整，请重新输入");
             return;
@@ -100,10 +100,14 @@ public class WriteFrame extends JFrame implements ActionListener, Runnable {
             if (fl.exists()) {
                 fw = new FileWriter(fl, true);
             } else {
-                fl.createNewFile();
-                fw = new FileWriter(fl);
+                if (fl.createNewFile()) {
+                    fw = new FileWriter(fl);
+                } else {
+                    status.setText("创建文件失败");
+                    return;
+                }
             }
-            fw.write(num_t.getText() + "," + name_t.getText() + "," + sex_t.getText() + "," + clas_t.getText() + ","
+            fw.write(num_t.getText() + "," + name_t.getText() + "," + sex_t.getText() + "," + cls_t.getText() + ","
                     + cn_t.getText() + "," + math_t.getText() + "," + eng_t.getText() + "," + pe_t.getText() + "\n");
             fw.flush();
             fw.close();
@@ -111,14 +115,14 @@ public class WriteFrame extends JFrame implements ActionListener, Runnable {
             e.printStackTrace();
         }
         status.setText("写入成功");
-        Clear_Textfield();
+        Clear_text();
     }
 
-    void Clear_Textfield() {
+    void Clear_text() {
         num_t.setText("");
         name_t.setText("");
         sex_t.setText("");
-        clas_t.setText("");
+        cls_t.setText("");
         cn_t.setText("");
         math_t.setText("");
         eng_t.setText("");
