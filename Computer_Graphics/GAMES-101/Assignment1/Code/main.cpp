60d02215da1d3d7a4b9bec1f1aeaf3032df3fb6b
MagicXin <x.yu2000@163.com>
2021-02-06 23:24:53 +0800

    update Assignment1

--- Computer_Graphics/GAMES-101/Assignment1/Code/main.cpp
+++ Computer_Graphics/GAMES-101/Assignment1/Code/main.cpp
@@ -11,8 +11,10 @@ Eigen::Matrix4f get_view_matrix(Eigen::Vector3f eye_pos)
     Eigen::Matrix4f view = Eigen::Matrix4f::Identity();
 
     Eigen::Matrix4f translate;
-    translate << 1, 0, 0, -eye_pos[0], 0, 1, 0, -eye_pos[1], 0, 0, 1,
-        -eye_pos[2], 0, 0, 0, 1;
+    translate << 1, 0, 0, -eye_pos[0],
+        0, 1, 0, -eye_pos[1],
+        0, 0, 1, -eye_pos[2],
+        0, 0, 0, 1;
 
     view = translate * view;
 
@@ -27,11 +29,11 @@ Eigen::Matrix4f get_model_matrix(float rotation_angle)
     // Create the model matrix for rotating the triangle around the Z axis.
     // Then return it.
 
-    float r = rotation_angle*MY_PI/180.0;
+    float r = rotation_angle * MY_PI / 180.0;
     model << std::cos(r), -std::sin(r), 0, 0,
-             std::sin(r), std::cos(r), 0, 0,
-             0, 0, 1, 0,
-             0, 0, 0, 1;
+        std::sin(r), std::cos(r), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1;
 
     return model;
 }
@@ -51,33 +53,56 @@ Eigen::Matrix4f get_projection_matrix(float eye_fov, float aspect_ratio,
 
     float n = zNear;
     float f = zFar;
-    float t = abs(n) * std::tan(eye_fov*MY_PI/180.0/2.0);
+    float t = abs(n) * std::tan(eye_fov * MY_PI / 180.0 / 2.0);
     float b = -t;
     float r = t * aspect_ratio;
     float l = -r;
-    ortho << 2.0/(r-l), 0, 0, -(r+l)/(r-l),
-             0, 2.0/(t-b), 0, -(t+b)/(t-n),
-             0, 0, 2.0/(n-f), -(n+f)/(n-f),
-             0, 0, 0, 1;
+    ortho << 2.0 / (r - l), 0, 0, -(r + l) / (r - l),
+        0, 2.0 / (t - b), 0, -(t + b) / (t - n),
+        0, 0, 2.0 / (n - f), -(n + f) / (n - f),
+        0, 0, 0, 1;
     persp << n, 0, 0, 0,
-             0, n, 0, 0,
-             0, 0, n + f, -n * f,
-             0, 0, 1, 0;
-    projection = ortho*persp;
+        0, n, 0, 0,
+        0, 0, n + f, -n * f,
+        0, 0, 1, 0;
+    projection = ortho * persp;
 
     return projection;
 }
 
-int main(int argc, const char** argv)
+// extra
+Eigen::Matrix4f get_rotation(Vector3f axis, float angle)
+{
+    Eigen::Matrix3f I = Eigen::Matrix3f::Identity();
+    Eigen::Matrix3f N, R;
+    float r = angle * MY_PI / 180.0;
+
+    N << 0, -axis[2], axis[1],
+        axis[2], 0, -axis[0],
+        -axis[1], axis[0], 0;
+    R = std::cos(r) * I + (1 - std::cos(r)) * axis * axis.transpose() + std::sin(r) * N;
+
+    Eigen::Matrix4f rotation;
+    rotation << R(0, 0), R(0, 1), R(0, 2), 0,
+        R(1, 0), R(1, 1), R(1, 2), 0,
+        R(2, 0), R(2, 1), R(2, 2), 0,
+        0, 0, 0, 1;
+
+    return rotation;
+}
+
+int main(int argc, const char **argv)
 {
     float angle = 0;
     bool command_line = false;
     std::string filename = "output.png";
 
-    if (argc >= 3) {
+    if (argc >= 3)
+    {
         command_line = true;
         angle = std::stof(argv[2]); // -r by default
-        if (argc == 4) {
+        if (argc == 4)
+        {
             filename = std::string(argv[3]);
         }
         else
@@ -98,10 +123,12 @@ int main(int argc, const char** argv)
     int key = 0;
     int frame_count = 0;
 
-    if (command_line) {
+    if (command_line)
+    {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
-        r.set_model(get_model_matrix(angle));
+        //r.set_model(get_model_matrix(angle));
+        r.set_model(get_rotation(Vector3f(0.0f, 0.0f, 1.0f), angle));
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
@@ -114,10 +141,12 @@ int main(int argc, const char** argv)
         return 0;
     }
 
-    while (key != 27) {
+    while (key != 27)
+    {
         r.clear(rst::Buffers::Color | rst::Buffers::Depth);
 
-        r.set_model(get_model_matrix(angle));
+        //r.set_model(get_model_matrix(angle));
+        r.set_model(get_rotation(Vector3f(1.0f, 0.0f, 0.0f), angle));
         r.set_view(get_view_matrix(eye_pos));
         r.set_projection(get_projection_matrix(45, 1, 0.1, 50));
 
@@ -130,10 +159,12 @@ int main(int argc, const char** argv)
 
         std::cout << "frame count: " << frame_count++ << '\n';
 
-        if (key == 'a') {
+        if (key == 'a')
+        {
             angle += 10;
         }
-        else if (key == 'd') {
+        else if (key == 'd')
+        {
             angle -= 10;
         }
     }
