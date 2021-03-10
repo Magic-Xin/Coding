c3d70f52b99cf2c0b9f2db78ad8f5335059e6514
MagicXin <x.yu2000@163.com>
2021-03-10 21:29:02 +0800

    update

---
+++ Computer_Graphics/NCUT/EXTRA/Triangle sample/Triangle sample/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Triangle sample
+//
+//  Created by MagicXin on 2021/3/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
