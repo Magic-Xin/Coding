9d468252201fabbeb3a49a6fb35271cf603f2295
MagicXin <x.yu2000@163.com>
2021-03-10 13:56:14 +0800

    update

--- Computer_Graphics/NCUT/CH02/Test1/Test1/main.cpp
+++ Computer_Graphics/NCUT/CH02/Test1/Test1/main.cpp
@@ -4,11 +4,69 @@
 //
 //  Created by MagicXin on 2021/3/10.
 //
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
 
-#include <iostream>
+#include <cstdlib>
+#include <GLUT/glut.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#define WHITE glColor3f(1.0f, 1.0f, 1.0f)
+#define RED glColor3f(1.0f, 0.0f, 0.0f)
+#define GREEN glColor3f(0.0f, 1.0f, 0.0f)
+#define BLUE glColor3f(0.0f, 0.0f, 1.0f)
+#define EXIT exit(0)
+
+int COLOR = 0;
+
+void SwitchColor(){
+    switch (COLOR) {
+        case 0:
+            WHITE;
+            break;
+        case 1:
+            RED;
+            break;
+        case 2:
+            GREEN;
+            break;
+        case 3:
+            BLUE;
+            break;
+        case 4:
+            EXIT;
+    }
+    glutPostRedisplay();
+}
+
+void RenderScene(){
+    glClear(GL_COLOR_BUFFER_BIT);
+    SwitchColor();
+    glRectf(-0.5, -0.5, 0.5, 0.5);
+    glFlush();
+}
+
+void MenuProcess(int value){
+    COLOR = value;
+}
+
+void CreateMenu(){
+    glutCreateMenu(MenuProcess);
+    glutAddMenuEntry("White", 0);
+    glutAddMenuEntry("Red", 1);
+    glutAddMenuEntry("Green", 2);
+    glutAddMenuEntry("Blue", 3);
+    glutAddMenuEntry("Exit", 4);
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
+}
+
+void init(){
+    glutCreateWindow("CH02 Test1");
+    glutDisplayFunc(RenderScene);
+    CreateMenu();
+    glutMainLoop();
+}
+
+int main(int argc, char* argv[]){
+    glutInit(&argc, argv);
+    init();
     return 0;
 }
