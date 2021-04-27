cc1d443bc324b9dc8ec0a0b0cca20c4aea1f7a03
Magic-Xin <mail@magicxin.tech>
2021-04-27 12:25:16 +0800

    update

--- Computer_Graphics/Taichi/Test/test.py
+++ Computer_Graphics/Taichi/Test/test.py
@@ -3,7 +3,7 @@ import tina
 
 ti.init(ti.gpu)
 
-scene = tina.Scene()
+scene = tina.Scene(fxaa=True)
 
 material = tina.PBR(metallic=0.6, roughness=0.2)
 model = tina.MeshModel('./monkey.obj')
@@ -17,7 +17,17 @@ scene.lighting.add_light(dir=[0, 0, 1], color=[1, 1, 1])
 scene.lighting.add_light(pos=[1, 1.5, 0.3], color=[1, 0, 0])
 scene.lighting.set_ambient_light([0, 0.06, 0])
 
+abs_thresh = gui.slider('abs_thresh', 0, 0.1, 0.002)
+rel_thresh = gui.slider('rel_thresh', 0, 0.5, 0.01)
+factor = gui.slider('factor', 0, 1, 0.01)
+abs_thresh.value = 0.0625
+rel_thresh.value = 0.063
+factor.value = 1
+
 while gui.running:
+    scene.fxaa.rel_thresh[None] = rel_thresh.value
+    scene.fxaa.abs_thresh[None] = abs_thresh.value
+    scene.fxaa.factor[None] = factor.value
     scene.input(gui)
     scene.render()
     gui.set_image(scene.img)
