1d1c0cc0d12ccb01c5d7562dbbc2880268160378
MagicXin <x.yu2000@163.com>
2021-05-10 21:25:21 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,13 +1,33 @@
 #include <stdio.h>
 
-int main() {
-    int sum = 0;
-
-    int j;
-    for(j = 1 ; j <= 10 ; j++) {
-
+int main()
+{
+    int i, n, l;
+    int a[3], b[3], c[3];
+    scanf("%d", &n);
+    for (i = 0; i < 3; i++)
+    {
+        scanf("%d%d", &a[i], &b[i]);
     }
 
-    printf("%d\n", sum);
+    for (i = 0; i < 3; i++)
+    {
+        c[i] = n / a[i] + 1;
+    }
+    if (c[0] > c[1])
+    {
+        if (c[1] > c[2])
+            l = c[1];
+        else
+            l = c[2];
+    }
+    else
+    {
+        if (c[1] > c[2])
+            l = c[1];
+        else
+            l = c[2];
+    }
+    printf("%d\n", l);
     return 0;
 }
