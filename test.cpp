a3b7403c8f592652263b3a42c4ccbb4b1e54ef12
Magic-Xin <mail@magicxin.tech>
2021-05-11 19:03:12 +0800

    update

--- test.cpp
+++ test.cpp
@@ -1,33 +1,16 @@
 #include <stdio.h>
-
 int main()
 {
-    int i, n, l;
-    int a[3], b[3], c[3];
-    scanf("%d", &n);
-    for (i = 0; i < 3; i++)
+    int l, m, all = 0, end;
+    int a[m - 1], b[m - 1], c[m - 1];
+    scanf("%d%d", &l, &m);
+    for (int i = 0; i <= m - 1; i++)
     {
         scanf("%d%d", &a[i], &b[i]);
+        c[i] = b[i] - a[i];
+        all += c[i];
     }
-
-    for (i = 0; i < 3; i++)
-    {
-        c[i] = n / a[i] + 1;
-    }
-    if (c[0] > c[1])
-    {
-        if (c[1] > c[2])
-            l = c[1];
-        else
-            l = c[2];
-    }
-    else
-    {
-        if (c[1] > c[2])
-            l = c[1];
-        else
-            l = c[2];
-    }
-    printf("%d\n", l);
+    end = l - all;
+    printf("%d\n", end);
     return 0;
 }
