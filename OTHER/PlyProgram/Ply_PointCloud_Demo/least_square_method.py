0cf6c243470939080a3dafcd38b8ab43cbe60039
MagicXin <x.yu2000@163.com>
2021-06-09 14:00:37 +0800

    update

--- OTHER/PlyProgram/Ply_PointCloud_Demo/least_square_method.py
+++ OTHER/PlyProgram/Ply_PointCloud_Demo/least_square_method.py
@@ -1,27 +1,5 @@
 import numpy as np
 from mayavi import mlab
-import matplotlib.pyplot as plt
-
-
-def show_matplotlib(x, y, z, res):
-    plt.figure()
-    ax = plt.subplot(111, projection='3d')
-    ax.scatter(x, y, z, color='b')
-
-    x_lim = ax.get_xlim()
-    y_lim = ax.get_ylim()
-    X, Y = np.meshgrid(np.arange(x_lim[0], x_lim[1]),
-                       np.arange(y_lim[0], y_lim[1]))
-    Z = np.zeros(X.shape)
-    for r in range(X.shape[0]):
-        for c in range(X.shape[1]):
-            Z[r, c] = res[0] * X[r, c] + res[1] * Y[r, c] + res[2]
-    ax.plot_wireframe(X, Y, Z, color='k')
-
-    ax.set_xlabel('x')
-    ax.set_ylabel('y')
-    ax.set_zlabel('z')
-    plt.show()
 
 
 def show_mayavi(x, y, z, res):
@@ -29,11 +7,13 @@ def show_mayavi(x, y, z, res):
     mlab.outline(color=(0, 0, 0))
 
     res = np.asarray(res)
-    xx, yy = np.mgrid[min(z):max(z):50j, min(z):max(z):50j]
+    xx, yy = np.mgrid[-10:10:0.5, -10:10:0.5]
     zz = res[0] * xx + res[1] * yy + res[2]
 
+    z /= 4
+
     mlab.surf(xx, yy, zz, color=(0, 0, 1), warp_scale=.25, representation='wireframe', line_width=0.5)
-    mlab.points3d(x, y, z, color=(1, 0, 0), mode='point', figure=fig)
+    mlab.points3d(x, y, z, color=(1, 0, 0), scale_factor=.3, figure=fig)
 
     axes = mlab.axes(color=(0, 0, 0), nb_labels=5)
     axes.title_text_property.color = (0.0, 0.0, 0.0)
@@ -46,9 +26,9 @@ def show_mayavi(x, y, z, res):
 
 
 def random_point(a, b, c):
-    x = np.random.uniform(-10, 10, size=50)
-    y = np.random.uniform(-10, 10, size=50)
-    z = (a * x + b * y + c) + np.random.normal(-1, 1, size=50)
+    x = np.random.uniform(-10, 10, size=100)
+    y = np.random.uniform(-10, 10, size=100)
+    z = (a * x + b * y + c) + np.random.normal(-1, 1, size=100)
     return x, y, z
 
 
@@ -66,9 +46,8 @@ def LSM(x, y, z):
 
 
 if __name__ == '__main__':
-    _x, _y, _z = random_point(2, 3, 5)
+    _x, _y, _z = random_point(2, 5, 6)
     _lsm = LSM(_x, _y, _z)
 
-    show_matplotlib(_x, _y, _z, _lsm)
     show_mayavi(_x, _y, _z, _lsm)
 
