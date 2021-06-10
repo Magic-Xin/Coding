a3f85d340fe8b2214d377bea5c68d8ddfb82cf7c
MagicXin <x.yu2000@163.com>
2021-06-10 23:19:54 +0800

    update

--- OTHER/PlyProgram/Ply_PointCloud_Demo/least_square_method.py
+++ OTHER/PlyProgram/Ply_PointCloud_Demo/least_square_method.py
@@ -1,5 +1,6 @@
 import numpy as np
 from mayavi import mlab
+from plyfile import PlyData
 
 
 def show_mayavi(x, y, z, res):
@@ -10,10 +11,11 @@ def show_mayavi(x, y, z, res):
     xx, yy = np.mgrid[-10:10:0.5, -10:10:0.5]
     zz = res[0] * xx + res[1] * yy + res[2]
 
-    z /= 4
+    # z /= 4
 
-    mlab.surf(xx, yy, zz, color=(0, 0, 1), warp_scale=.25, representation='wireframe', line_width=0.5)
-    mlab.points3d(x, y, z, color=(1, 0, 0), scale_factor=.3, figure=fig)
+    # mlab.surf(xx, yy, zz, color=(0, 0, 1), warp_scale=.25, representation='wireframe', line_width=0.5)
+    # mlab.points3d(x, y, z, color=(1, 0, 0), scale_factor=.3, figure=fig)
+    mlab.points3d(x, y, z, color=(1, 0, 0), mode='point', figure=fig)
 
     axes = mlab.axes(color=(0, 0, 0), nb_labels=5)
     axes.title_text_property.color = (0.0, 0.0, 0.0)
@@ -32,6 +34,13 @@ def random_point(a, b, c):
     return x, y, z
 
 
+def read_ply(filepath):
+    _data = PlyData.read(filepath)
+    _pc = _data['vertex'].data
+    _array = np.array([_pc['x'], _pc['y'], _pc['z']])
+    return _array
+
+
 def LSM(x, y, z):
     tmp_a = []
     tmp_b = []
@@ -46,7 +55,8 @@ def LSM(x, y, z):
 
 
 if __name__ == '__main__':
-    _x, _y, _z = random_point(2, 5, 6)
+    # _x, _y, _z = random_point(2, 5, 6)
+    _x, _y, _z = read_ply('./1.ply')
     _lsm = LSM(_x, _y, _z)
 
     show_mayavi(_x, _y, _z, _lsm)
--- OTHER/PlyProgram/Ply_PointCloud_Demo/mayavi_ply.py
+++ OTHER/PlyProgram/Ply_PointCloud_Demo/mayavi_ply.py
@@ -1,34 +1,52 @@
 import numpy as np
 import mayavi.mlab as mlab
+import open3d as o3d
 from plyfile import PlyData
 
 
 def read_ply(filepath):
-    _data = PlyData.read(filepath)
-    _pc = _data['vertex'].data
-    _array = np.array([_pc['x'], _pc['y'], _pc['z']])
-    return _array
+    pcd = o3d.io.read_point_cloud(filepath)
+    print(pcd)
+    return pcd
 
 
-def draw_ply():
-    mlab.figure(bgcolor=(0.2, 0.3, 0.3), fgcolor=(1.0, 0.5, 0.3))
-    ply_array = read_ply('./1.ply')
-    x = ply_array[0]
-    y = ply_array[1]
-    z = ply_array[2]
+def draw_ply(array_in, array_out):
+    mlab.figure(bgcolor=(1, 1, 1), size=(640, 480))
+    mlab.outline(color=(0, 0, 0))
 
-    mlab.points3d(x, y, z, scale_factor=.02)
+    mlab.points3d(array_in[:, 0], array_in[:, 1], array_in[:, 2], color=(1, 0, 0), mode='point')
+    mlab.points3d(array_out[:, 0], array_out[:, 1], array_out[:, 2], color=(0, 0, 1), mode='point')
+
+    axes = mlab.axes(color=(0, 0, 0), nb_labels=5)
+    axes.title_text_property.color = (0.0, 0.0, 0.0)
+    axes.title_text_property.font_family = 'times'
+    axes.label_text_property.color = (0.0, 0.0, 0.0)
+    axes.label_text_property.font_family = 'times'
+    mlab.gcf().scene.parallel_projection = True
+    mlab.orientation_axes()
     mlab.show()
 
 
-def draw_line():
-    mlab.figure(bgcolor=(0.2, 0.3, 0.3), fgcolor=(1.0, 0.5, 0.3))
-    mlab.plot3d([0, 1000], [0, 0], [0, 0], tube_radius=10.)
-    mlab.plot3d([0, 0], [0, 1500], [0, 0], tube_radius=10.)
-    mlab.plot3d([0, 0], [0, 0], [0, 1500], tube_radius=10.)
-    mlab.show()
+def pcFunc(pcd):
+    pcd_new = o3d.geometry.PointCloud.remove_non_finite_points(pcd, remove_nan=True, remove_infinite=True)
+    pcd_new = o3d.geometry.PointCloud.remove_radius_outlier(pcd_new, 2, 0.1)
+    print("After radius: ")
+    print(pcd_new[0])
+    # pcd_new = o3d.geometry.PointCloud.remove_statistical_outlier(pcd_new[0], 3, 0.2)
+    # print("After statistical: ")
+    # print(pcd_new[0])
+    return pcd_new[0]
+
+
+def ransacFunc(pcd):
+    plane_model, inliers = pcd.segment_plane(distance_threshold=.1, ransac_n=10, num_iterations=20)
+    pcd_in = pcd.select_by_index(inliers)
+    pcd_out = pcd.select_by_index(inliers, invert=True)
+    return np.asarray(pcd_in.points), np.asarray(pcd_out.points)
 
 
 if __name__ == '__main__':
-    draw_ply()
-    # draw_line()
+    _pcd = read_ply('1.ply')
+    _pcd = pcFunc(_pcd)
+    _array_in, _array_out = ransacFunc(_pcd)
+    draw_ply(_array_in, _array_out)
