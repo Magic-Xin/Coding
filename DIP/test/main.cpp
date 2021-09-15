3a973f47f663c177f1383c6d9c88308f33f4e9bf
MagicXin <x.yu2000@163.com>
2021-09-15 23:20:25 +0800

    update DIP

--- DIP/test/main.cpp
+++ DIP/test/main.cpp
@@ -3,6 +3,13 @@
 
 int main() {
     cv::Mat img;
-    img = cv::re
+    img = cv::imread("../img.jpg");
+    if (img.empty())
+    {
+        std::cout << "file not find!" << std::endl;
+        return -1;
+    }
+    cv::imshow("waifu!", img);
+    cv::waitKey(0);
     return 0;
 }
